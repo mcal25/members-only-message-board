6918da72daea3db2213491faf09834bb5deadbf7
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-08-28 18:26:15 -0400

    Make index dynamically update based on login status

--- app.js
+++ app.js
@@ -12,25 +12,27 @@ import indexRouter from "./routes/indexRouter.js";
 import usersRouter from "./routes/usersRouter.js";
 import messagesRouter from "./routes/messagesRouter.js";
 
-
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false}));
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/messages', messagesRouter);
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
+app.use("/messages", messagesRouter);
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.listen(3000, (error) => {
-    if (error) {
-        throw error;
-    }
-    console.log("Listening on port 3000.");
-})
+  if (error) {
+    throw error;
+  }
+  console.log("Listening on port 3000.");
+});
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { user: req.user });
 });
 
 export default indexRouter;
