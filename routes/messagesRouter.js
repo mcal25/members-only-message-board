2dcc41d10e262597f90697027768c2fa1bc196d4
mcal25 <225284010+mcal25@users.noreply.github.com>
2026-09-02 18:40:26 -0400

    Make delete message work with admin permission check

--- routes/messagesRouter.js
+++ routes/messagesRouter.js
@@ -10,30 +10,57 @@ const messagesRouter = Router();
 
 messagesRouter.get("/", async (req, res) => {
   const allMessages = await getAllMessages();
-  res.render("messages", { user: res.locals.user, allMessages: allMessages });
+  res.render("messages", { user: req.user, allMessages: allMessages });
 });
 
 messagesRouter.get("/new-message", (req, res) => {
-    res.render("new-message");
+  res.render("new-message");
 });
 
 messagesRouter.get("/one-of-us", (req, res) => {
-    res.render("one-of-us");
+  res.render("one-of-us");
 });
 
 messagesRouter.post("/one-of-us", async (req, res) => {
-    if (req.body.membercode == process.env.MEMBERSHIP_CODE) {
-        console.log('THEY SHALL BECOME ONE OF US');
-        console.log(req.user);
-        await pool.query(`
+  console.log("HITTING BEFORE THE IFFY");
+  if (req.body.membercode == process.env.MEMBERSHIP_CODE) {
+    console.log("THEY SHALL BECOME ONE OF US");
+    console.log(req.user);
+    await pool.query(
+      `
             UPDATE users 
             SET membership_status = true 
             WHERE username = $1;
         `,
-        [req.user.username]);
-    }
+      [req.user.username],
+    );
+  } else if (req.body.admincode == process.env.ADMIN_CODE) {
+    await pool.query(
+      `
+            UPDATE users
+            SET admin_status = true
+            WHERE username = $1;    
+        `,
+      [req.user.username],
+    );
+  }
 
-    res.redirect('/');
+  res.redirect("/");
+});
+
+// The form using this function has a POST method attribute because html form elements cannot accept DELETE
+messagesRouter.post("/delete-message", async (req, res) => {
+  console.log("I WILL DELETE YOU AND YOUR WHOLE FAMILY");
+  if (req.user.admin_status) {
+    await pool.query(
+      `
+        DELETE FROM messages
+        WHERE id = $1
+    `,
+      [req.body.messageid],
+    );
+  }
+  res.redirect("/messages");
 });
 
 export default messagesRouter;
